62ba532e3d9d74a414224e75d7eea189ace0dafe
irving <irving1364@gmail.com>
2024-05-17 22:42:27 -0400

    'update'

--- src/components/seo.jsx
+++ src/components/seo.jsx
@@ -8,7 +8,7 @@ const SEO = ({ pageTitle }) => {
             <title>{title}</title>
             <meta httpEquiv="x-ua-compatible" content="ie=edge" />
             <meta name="description" content="Potencia tu Rendimiento" />
-            <meta name="robots" content="noindex, follow" />
+            
             <meta
                 name="viewport"
                 content="width=device-width, initial-scale=1, shrink-to-fit=no"
