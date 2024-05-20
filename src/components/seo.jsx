559137f6820a74480a6c1c8a836b7022de4ef614
irving <irving1364@gmail.com>
2024-05-20 07:30:39 -0400

    'update'

--- src/components/seo.jsx
+++ src/components/seo.jsx
@@ -5,18 +5,27 @@ const SEO = ({ pageTitle }) => {
     const title = `${pageTitle} || Puro Musculos`;
     return (
         <Head>
-            <title>{title}</title>
+            <title>Puro Musculos | {pageTitle}</title>
+            <meta property="og:title" content="Puro Musculos | Potencia tu Rendimiento "></meta>
+            <meta property="og:type" content="website" ></meta>
+            <meta property="og:url" content="https://www.puromusculos.com" ></meta>
+            <meta lang = "es"></meta>
+            <meta property="og:site_name"  content="Puro Musculos"></meta>
+
             <meta httpEquiv="x-ua-compatible" content="ie=edge" />
             <meta name="description" content="Potencia tu Rendimiento | Ventas online de productos de pontenciadores musculares | Sarms | Inyectables | Orales | Hgh-peptido | Quema Grasas | Ciclos" />
+            <meta property="og:description" content="Potencia tu Rendimiento | Ventas online de productos de pontenciadores musculares | Sarms | Inyectables | Orales | Hgh-peptido | Quema Grasas | Ciclos"></meta>
+            <meta name="keywords" content="Puro Musculos, Potencia tu Rendimiento, Sarms , Inyectables , Orales , Hgh-peptido , Quema Grasas , Ciclos"></meta> 
+            <meta name="author" content="Irving salcedo - irvng1364@gmail.com"></meta>          
             <meta name="google-site-verification" content="_cMogX_DIPquudlQWQo1Fx0uPtI4ncWINi-erlnxWqM" />
             <meta
                 name="viewport"
                 content="width=device-width, initial-scale=1, shrink-to-fit=no"
             />
             <link rel="icon" href="/Logo-Puro-Musculos-negro.png" />
-
-            <meta property="og:url" content="https://www.fadimet.com.pa" ></meta>
             <meta property="og:image" content="/Logo-Puro-Musculos-negro.png"></meta>
+            <meta http-equiv="X-UA-Compatible" content="IE=edge"></meta>
+            <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"></meta>
 
         </Head>
     );
